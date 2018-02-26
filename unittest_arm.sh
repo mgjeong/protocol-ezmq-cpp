3c2e69f83f63e56559d8e0312f2aae281a9dfd46
bg.chun <bg.chun@samsung.com>
2018-02-26 14:02:09 +0900

    patch for HQ CI/CD system
    Signed-off-by: bg.chun <bg.chun@samsung.com>
    Signed-off-by: Jay Sharma <jay.sharma@samsung.com>

---
+++ unittest_arm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./build_auto.sh --target_arch=armhf-qemu
+
+cd out/linux/armhf/release/unittests
+
+#ezmq_api_test
+./ezmq_api_test
+
+#ezmq_pub_test
+./ezmq_pub_test
+
+#ezmq_sub_test
+./ezmq_sub_test
+
+#ezmq_byteData_test
+./ezmq_byteData_test
+
