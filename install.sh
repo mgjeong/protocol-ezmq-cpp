8aeb1260cc04b496b5c7921a7e286e825d881d57
박진혁/Data Service Lab(SR)/Engineer/삼성전자 <jinhyuck83.park@samsung.com>
2018-04-27 13:11:47 +0900

    Update install.sh
    update install.sh because of "tmp" is not exists

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+USR_LIB_DIR=/usr/local/lib
+USR_INC_DIR=/usr/local/include
+KEYWORD_EZMQ_CPP=ezmq-cpp
+
+# COPY shared library to /usr/local/lib
 sudo cp out/linux/x86_64/release/libezmq.so /usr/local/lib
-cp -r include tmp
-cp protobuf/* tmp/
-sudo cp -r  tmp /usr/local/include/ezmq-cpp
+
+# COPY header file to /usr/local/include/opcua-c
+sudo rm -rf $USR_INC_DIR/$KEYWORD_EZMQ_CPP
+sudo mkdir $USR_INC_DIR/$KEYWORD_EZMQ_CPP
+sudo cp -f ./include/* $USR_INC_DIR/$KEYWORD_EZMQ_CPP/
