eea5a499b537e57f4d574f9b609b6220300665b3
bg.chun <bg.chun@samsung.com>
2018-05-09 13:27:48 +0900

    update install script
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- install_arm.sh
+++ install_arm.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+USR_LIB_DIR=/usr/local/lib
+USR_INC_DIR=/usr/local/include
+KEYWORD_EZMQ_CPP=protocol-ezmq-cpp
+
+# COPY shared library to /usr/local/lib
 sudo cp out/linux/armhf/release/libezmq.so /usr/local/lib
-cp -r include tmp
-cp protobuf/* tmp/
-sudo cp -r  tmp /usr/local/include/ezmq-cpp
+
+# COPY header file to /usr/local/include/protocol-ezmq-cpp
+sudo rm -rf $USR_INC_DIR/$KEYWORD_EZMQ_CPP
+sudo mkdir $USR_INC_DIR/$KEYWORD_EZMQ_CPP
+mkdir tmp
+cp -rf ./protobuf/* ./tmp/
+cp -rf ./include/* ./tmp/
+sudo cp -rf ./tmp/* $USR_INC_DIR/$KEYWORD_EZMQ_CPP/
+rm -rf ./tmp
